29e99428dd12ec927676e59f43d4beb5afd3c68f
alquin <aeledana@gmail.com>
2023-03-20 13:39:48 +0800

    swecond commit

---
+++ functions.php
@@ -0,0 +1,28 @@
+<?php 
+function load_stylesheets(){
+    wp_register_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), false, 'all');
+    wp_enqueue_style('bootstrap');
+    wp_register_style('style', get_template_directory_uri() . '/style.css', array(), false, 'all');
+    wp_enqueue_style('style');
+}
+add_action( 'wp_enqueue_scripts', 'load_stylesheets' );
+
+function include_jquery()
+{
+        wp_deregister_script('jquery');
+        wp_enqueue_script('jquery', get_template_directory_uri() . '/js/jquery-3.6.4.min.js', '', 1, true);
+        add_action('wp_enqueue_scripts', 'jquery');
+}
+add_action('wp_enqueue_scripts', 'include_jquery');
+
+function include_js()
+{
+        wp_register_script('customjs', get_template_directory_uri() . '/js/scripts.js', '', 1, true);
+        wp_enqueue_script('customjs');
+
+        wp_register_script('bootstrapjs', get_template_directory_uri() . '/js/bootstrap.min.js', '', 1, true);
+        wp_enqueue_script('bootstrapjs');
+
+        
+}
+add_action('wp_enqueue_scripts', 'include_js');
